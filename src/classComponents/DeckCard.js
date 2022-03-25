90a4f3ccfee92882e280f39ca565312cddf0317f
sulizbasnet <sulizzbasnet@gmail.com>
2022-03-25 15:57:02 -0700

    added gestures and animations

--- src/classComponents/DeckCard.js
+++ src/classComponents/DeckCard.js
@@ -1,10 +1,54 @@
-import { Text, View } from "react-native";
+import {
+    Text,
+    View,
+    PanResponder,
+    Animated,
+    TurboModuleRegistry,
+} from "react-native";
 import React, { Component } from "react";
 
 export default class DeckCard extends Component {
+    constructor(props) {
+        super(props);
+
+        const position = new Animated.ValueXY();
+
+        const panResponder = PanResponder.create({
+            // Ask to be the responder:
+            //function is exectued anytime a user taps on the screen. Press down.
+            onStartShouldSetPanResponder: () => true,
+
+            //callback will be called as user is pressing and dragging.
+            onPanResponderMove: (event, gestureState) => {
+                // The most recent move distance is gestureState.move{X,Y}
+                // The accumulated gesture distance since becoming responder is
+                // gestureState.d{x,y}
+                position.setValue({ x: gestureState.dx, y: gestureState.dy });
+            },
+
+            //user presses down and drag and release.
+            onPanResponderRelease: () => {
+                // The user has released all touches while this view is the
+                // responder. This typically means a gesture has succeeded
+            },
+        });
+
+        this.state = { panResponder, position };
+    }
+
     renderCards() {
-        return this.props.data.map((item) => {
-            return this.props.renderCard(item);
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.state.position.getLayout()}
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
         });
     }
     render() {
